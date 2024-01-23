40a6e0639ed9a5a3d1e11a21a1bdaf88b73fb759
Nored <myaminored@gmail.com>
2024-01-23 00:30:09 -0500

    finishing ex9

--- shaw/ex8/ex8.c
+++ shaw/ex8/ex8.c
@@ -4,18 +4,19 @@ int main(int argc, char *argv[])
 {
 	int i = 0 ;
 
-	if (argc == 1) {
-		printf("You only have one aregument. you suck.\n");
-} else if (argc > 1 && argc <4){
+	if (argc == 2) {
+		printf("You only two arguments the first is:  %s. \n and the second is:  %s\t .\n",argv[0],argv[1]);
+
+} else if (argc > 1 || argc <4){
 	printf("Here's your arguments:\n");
 
 	for (i = 0 ; i < argc ; i++) {
 		printf("%s\t" , argv[i]);
 	}
 	printf("\n");
-	} else {
+	}/* else {
 		printf("You have too many arguments. You suck .\n");
 	}
-
+		*/
 	return 0;
 	}
