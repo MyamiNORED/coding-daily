4bff349dfa8f887946f9b6f7b128c5c33a6e011d
Nored <myaminored@gmail.com>
2024-08-09 17:08:10 -0400

    did string methods,and started stanford c++ library

--- cs106b/week_one/stringops/main.cpp
+++ cs106b/week_one/stringops/main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 int main()
 {
-    string s = "hello there";
+    string s = "elephant";
 
     // Notice that we are not passing s to a length function. Rather, we use the dot
     // operator on the s variable to call a length() function that is packed up with
     // the string and which gives us the length of that string (11, as there are 11
     // characters in "hello there"). (The space counts a character.)
 
-    cout << s.length() << endl;
+    //cout << s.length() << endl;
+    //cout << s.append(" test") << endl;
+    // cout << s.compare("elephanu") << endl ;
+     cout << s.find("xu");
+
+
 
     return 0;
 }
